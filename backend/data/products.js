daca74afc95da29ee0566ca20471e5d320adf65f
YUN GU <nerokucn@gmail.com>
2024-04-16 21:10:07 -0700

    1

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'The Catcher in the Rye',
+    image: '/images/catcher_in_the_rye.jpg',
+    author: 'J.D. Salinger',
+    description:
+      'The Catcher in the Rye is a novel by J. D. Salinger. Originally published for adults, it has become popular among adolescents for its themes of teenage angst and alienation.',
+    category: 'Literature & Fiction',
+    price: 9.99,
+    countInStock: 20,
+    rating: 4.2,
+    numReviews: 1528,
+  },
+  {
+    name: 'To Kill a Mockingbird',
+    image: '/images/to_kill_a_mockingbird.jpg',
+    author: 'Harper Lee',
+    description:
+      'The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it, To Kill A Mockingbird became both an instant bestseller and a critical success when it was first published in 1960.',
+    category: 'Literature & Fiction',
+    price: 7.99,
+    countInStock: 15,
+    rating: 4.7,
+    numReviews: 2345,
+  },
+  {
+    name: 'The Great Gatsby',
+    image: '/images/the_great_gatsby.jpg',
+    author: 'F. Scott Fitzgerald',
+    description:
+      'The Great Gatsby, F. Scott Fitzgerald\'s third book, stands as the supreme achievement of his career. This exemplary novel of the Jazz Age has been acclaimed by generations of readers.',
+    category: 'Literature & Fiction',
+    price: 6.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 1789,
+  },
+  {
+    name: "Harry Potter and the Sorcerer's Stone",
+    image: '/images/harry_potter_1.jpg',
+    author: 'J.K. Rowling',
+  description:
+  'Harry Potter has never played a sport while flying on a broomstick. He\'s never worn a cloak of invisibility, befriended a giant, or helped hatch a dragon. All Harry knows is a miserable life with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley.',
+    category: 'Fantasy',
+    price: 8.99,
+    countInStock: 25,
+    rating: 4.8,
+    numReviews: 4567,
+  },
+  {
+    name: 'The Alchemist',
+    image: '/images/the_alchemist.jpg',
+    author: 'Paulo Coelho',
+    description:
+      'The Alchemist by Paulo Coelho continues to change the lives of its readers forever. With more than two million copies sold around the world, The Alchemist has established itself as a modern classic, universally admired.',
+    category: 'Fiction',
+    price: 11.99,
+    countInStock: 18,
+    rating: 4.3,
+    numReviews: 2345,
+  },
+  {
+    name: '1984',
+    image: '/images/1984.jpg',
+    author: 'George Orwell',
+    description:
+      "Among the seminal texts of the 20th century, Nineteen Eighty-Four is a rare work that grows more haunting as its futuristic purgatory becomes more real. Published in 1949, the book offers political satirist George Orwell's nightmarish vision of a totalitarian, bureaucratic world and one poor stiff's attempt to find individuality.",
+    category: 'Science Fiction',
+    price: 9.99,
+    countInStock: 12,
+    rating: 4.6,
+    numReviews: 3456,
+  },
+];
+
+export default products;
